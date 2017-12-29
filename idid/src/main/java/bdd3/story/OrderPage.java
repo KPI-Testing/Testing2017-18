2d38e35cbc9492e7a47d5cbe00304ed941ab2635
vlrie <30659485+vlrie@users.noreply.github.com>
2017-12-29 15:29:45 +0200

    8lab

---
+++ idid/src/main/java/bdd3/story/OrderPage.java
@@ -0,0 +1,31 @@
+package bdd3.story;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+public class OrderPage {
+	private WebDriver driver;
+
+	   @FindBy(xpath = ".//*[@id='make-order']")
+
+	   private WebElement Checkout;
+
+	   //Constructor
+	   public OrderPage (WebDriver driver){
+	       this.driver=driver;
+
+	       //Initialise Elements
+	       PageFactory.initElements(new AjaxElementLocatorFactory(driver, 25), this);
+	   }
+
+
+	   public Boolean IsPassed(){
+		   if (Checkout.isDisplayed() && Checkout.isEnabled()) return true;
+		   else
+		   return false;   
+
+	   }
+}
---
+++ idid/src/main/java/bdd3/story/SchoolBooksPage.java
@@ -0,0 +1,40 @@
+package bdd3.story;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+
+public class SchoolBooksPage {
+	private WebDriver driver;
+
+	private static String PAGE_URL="https://rozetka.com.ua/office-school-books/c4625734/";
+	
+	   @FindBy(xpath = "//a[contains(@href, 'http://rozetka.com.ua/school_bags/c163943/')]") 
+	   private WebElement SchoolBagPage;
+
+	   @FindBy(xpath = "//a[contains(@href, 'https://my.rozetka.com.ua/cart/')]")
+	   private WebElement BasketPage;
+
+
+	   //Constructor
+	   public SchoolBooksPage (WebDriver driver){
+       this.driver=driver;
+       driver.manage().deleteAllCookies();
+       driver.get(PAGE_URL);
+
+	       //Initialise Elements
+	       PageFactory.initElements(new AjaxElementLocatorFactory(driver, 25), this);
+	   }
+
+
+	   public void clikSchoolBagPage(){
+		   SchoolBagPage.click();
+	   }
+	  public void clickOnBasketPage(){
+			BasketPage.click();
+   }
+	   
+}
