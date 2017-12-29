467cf2df8f07338bab08f7cabaa44e87af546607
unknown <vukpud@gmail.com>
2017-12-29 13:36:20 +0200

    lab6

---
+++ Lab6 Konopatska Sandra/Lab6/RozetkaFrameworkTest/TestFramework/Pages/OneQuadrPage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace TestFramework.Pages
+{
+    public class OneQuadrPage
+    {
+        private IWebDriver _driver;
+
+        public OneQuadrPage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "price_label")]
+        public IWebElement Price;
+
+        public int? GetPrice()
+        {
+            var stringValue = Price.Text;
+            if (stringValue == null | stringValue == "")
+                return null;
+            else
+            {
+                int.TryParse(stringValue, out int result);
+                return result;
+            }
+        }
+
+    }
+}
